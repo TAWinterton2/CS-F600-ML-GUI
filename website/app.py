444f7086301b8572eb0446630523788392e9729d
raelewis <relewis@me.com>
2023-10-02 17:20:48 -0800

    Simple navigation
    Added the html file for the linear regression form. Additionally, allowed app.py and the nav bar to navigate to and from the home page/linear regression model. For any pages not implemented yet, it will navigate the user back to the home page.

--- website/app.py
+++ website/app.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
+    """Renders the home page of the website, the first page that a user will land on when visiting the website."""
     return render_template('index.html')
+
+
+@app.route("/linear")
+def ml_form():
+    """Renders the machine learning form for the linear regression model. This is done by pressing the button on the navigation bar."""
+    return render_template('linear.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
