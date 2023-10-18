29aa2a6cdd57549fadad629562fab823e9182e6a
raelewis <relewis@me.com>
2023-10-18 15:47:53 -0800

    Error handling
    Adjusted files post merge to properly display results after uploading and merging csv files. I also added an additional way to verify that the submitted files are of the correct extension, and return an error if they are not.
    
    Finally, I added a 'catch all' error handling to zip_unpack, in case there is an unknown error in our process, that prevents the server from crashing.

--- website/app.py
+++ website/app.py
@@ -10,6 +10,7 @@ class DataSnapshot():
     def __init__(self):
         self.og_data = None
         self.data = None
+        self.filename = None
 
     def select_columns(self, x, y):
         df = self.og_data[[x, y]].copy()
@@ -18,7 +19,16 @@ class DataSnapshot():
 
 snapshot = DataSnapshot()
 
+
+
 """Input Parsing Functions"""
+ALLOWED_EXTENSIONS = {'csv', 'zip'}
+def allowed_file(filename):
+    """Method to ensure that the user supplied a file with the correct extension."""
+    # https://flask.palletsprojects.com/en/2.3.x/patterns/fileuploads/
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 def csv_upload(file):
     """This function takes a file input and converts it to a pandas DataFrame."""
     df = pd.read_csv(file)
@@ -26,59 +36,62 @@ def csv_upload(file):
 
 def zip_unpack(zip_file):
    #Get current wording directory of server and save it as a string
-    cwd = os.getcwd()
-    zip_file_path = os.path.join(cwd, zip_file)
+    try:
+        cwd = os.getcwd()
+        zip_file_path = os.path.join(cwd, zip_file)
 
-    temp_dir = "temp"
-    temp_path = os.path.join(cwd, temp_dir)
+        temp_dir = "temp"
+        temp_path = os.path.join(cwd, temp_dir)
 
-    ext = ('.csv')
+        ext = ('.csv')
 
-    #check if temp already exists
-    if not os.path.exists(temp_path):
-        os.mkdir(temp_path)
+        #check if temp already exists
+        if not os.path.exists(temp_path):
+            os.mkdir(temp_path)
 
-    with ZipFile(zip_file_path, 'r') as zObject:
-    #Extract all files in the zip into a specific location
-        zObject.extractall(path=temp_path)
-        zObject.close()
+        with ZipFile(zip_file_path, 'r') as zObject:
+        #Extract all files in the zip into a specific location
+            zObject.extractall(path=temp_path)
+            zObject.close()
 
-    temp_folder_name = os.path.splitext(zip_file)[0]
-    tmp = temp_path
-    temp_path = os.path.join(temp_path, temp_folder_name)
+        temp_folder_name = os.path.splitext(zip_file)[0]
+        tmp = temp_path
+        temp_path = os.path.join(temp_path, temp_folder_name)
 
-    files = []
-    for file in os.listdir(temp_path):
-        # Mac
-        if not file.startswith('.'):
-            if file.endswith(ext):
-                files.append(file)
+        files = []
+        for file in os.listdir(temp_path):
+            # Mac
+            if not file.startswith('.'):
+                if file.endswith(ext):
+                    files.append(file)
 
-            else:
+                else:
+                    shutil.rmtree(tmp)
+                    os.remove(zip_file_path)
+                    os.chdir(cwd)
+                    return "There are files in " + zip_file + " that are not .csv files. Please try again with only .csv files."
+        
+        d = []
+        for f in files:
+            tmp_path = os.path.join(temp_path, f)
+            df = pd.read_csv(tmp_path, on_bad_lines='skip')
+            d.append(df)
+
+        columns = d[0].columns
+        for df in d:
+            if df.columns.difference(columns).empty is False:
                 shutil.rmtree(tmp)
                 os.remove(zip_file_path)
                 os.chdir(cwd)
-                return "There are files in " + zip_file + " that are not .csv files. Please try again with only .csv files."
-    
-    d = []
-    for f in files:
-        tmp_path = os.path.join(temp_path, f)
-        df = pd.read_csv(tmp_path, on_bad_lines='skip')
-        d.append(df)
-
-    columns = d[0].columns
-    for df in d:
-        if df.columns.difference(columns).empty is False:
-            shutil.rmtree(tmp)
-            os.remove(zip_file_path)
-            os.chdir(cwd)
-            return "The csv files within " + zip_file +" do not have the same column names. Please resubmit with .csvs that have matching columns."
-        # load all csv files, check if columns match, return either a dataframe or a string error
-    
-    shutil.rmtree(tmp)
-    os.remove(zip_file_path)
-    os.chdir(cwd)
-    return pd.concat(d, axis=0)
+                return "The csv files within " + zip_file +" do not have the same column names. Please resubmit with .csvs that have matching columns."
+            # load all csv files, check if columns match, return either a dataframe or a string error
+        
+        shutil.rmtree(tmp)
+        os.remove(zip_file_path)
+        os.chdir(cwd)
+        return pd.concat(d, axis=0)
+    except:
+        return "Something went wrong."
 
 
 def text_input_parse(s):
@@ -114,9 +127,6 @@ def index():
 
 
 @app.route("/linear", methods=['POST', 'GET'])
-
-
-
 def ml_form():
     """Renders the machine learning form for the linear regression model. This is done by pressing the button on the navigation bar."""
     if request.method == 'POST':
@@ -124,35 +134,41 @@ def ml_form():
         # Update this for WTForms later to better handle the data?
         if 'upload_file' in request.form:
             f = request.files['file']
-            f.save(f.filename)
-            if zipfile.is_zipfile(f):
-                result = zip_unpack(f.filename)
-                if isinstance(result, str):
-                    return render_template('ml_form.html',
-                           tab=0, 
-                           filename=request.files['file'].filename,
-                           error=result)
+            if allowed_file(f.filename):
+                f.save(f.filename)
+                if zipfile.is_zipfile(f):
+                    result = zip_unpack(f.filename)
+                    if isinstance(result, str):
+                        return render_template('ml_form.html',
+                            tab=0, 
+                            filename=request.files['file'].filename,
+                            error=result)
+                    else:
+                        snapshot.og_data = result
+
                 else:
-                    return render_template('ml_form.html',
-                           tab=0, 
-                           filename=request.files['file'].filename,
-                           error=type(result))
+                    df = csv_upload(request.files['file'])
+                    snapshot.og_data = df
+
+                return render_template('ml_form.html',
+                            tab=0, 
+                            file_upload=True,
+                            filename=request.files['file'].filename,
+                            og_df=snapshot.og_data.to_html(),
+                            column_names=snapshot.og_data.columns.tolist())
             else:
-                df = csv_upload(request.files['file'])
-            snapshot.og_data = df
-            return render_template('ml_form.html',
-                           tab=0, 
-                           file_upload=True,
-                           filename=request.files['file'].filename,
-                           og_df=snapshot.og_data.to_html(),
-                           column_names=snapshot.og_data.columns.tolist())
-        
+                return render_template('ml_form.html',
+                            tab=0, 
+                            filename=request.files['file'].filename,
+                            error="Please submit a file with a valid extension (csv or zip).")
+
         if 'select_xy' in request.form:
             snapshot.select_columns(request.form['X'], request.form['Y'])
             df = get_graph_data(snapshot.data)
             return render_template('ml_form.html',
                            tab=0, 
                            columns_selected=True,
+                           form_complete=True,
                            name="myChart",
                            data=df.to_json(orient="records"),
                            user_input=True,
