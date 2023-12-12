5dad3c31e4d47d0996b3436e3ee4d16bc1fece96
TAWinterton2 <tawinterton2@alaska.edu>
2023-12-11 22:05:21 -0500

    updated initalize() in supportvector.py

--- website/utils/supportvector.py
+++ website/utils/supportvector.py
@@ -3,10 +3,12 @@ from website.utils.model import Model
 
 class SupportVectorMachine(Model):
     def initalize(val):
+    
+        svm_model = svm.SVC( C=val[0], kernel=val[1], degree=val[2], gamma=val[3], coef0=val[4], shrinking=val[5],
+                             probability=val[6], tol=val[7], cache_size=val[8], class_weight=val[9],
+                             verbose=val[10], max_iter=val[11], decision_function_shape=val[12], break_ties=val[13], random_state=val[14])
         
-        clf = svm.SVC(kernel='linear', C= 10)
-        
-        return clf
+        return svm_model()
 
     def evaluate(y_test, y_pred):
 
